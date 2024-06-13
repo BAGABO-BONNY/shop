412b4c53cec421b4460e20d6326201d39b16aca8
BAGABO-BONNY <bagabobonny544@gmail.com>
2024-06-14 00:27:10 +0200

    fourth commit

--- shop/src/components/Category/Category.jsx
+++ shop/src/components/Category/Category.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import Button from '../shared/Button'
+import Image1 from'../hero/cupBoard.png'
 
 const Category = () => {
   return (
     <div className='py-8'>
         <div className='container'>
             <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
-                <div>
+                <div className='py-10 pl-5 bg-gradient-to-br from-black to-black/70 text-white rounded-3xl relative h-[320px] flex items-end'>
                     <div>
-                        <div>
+                        <div className='mb-4'>
                             <p>Enjoy</p>
                             <p>With</p>
-                            <p>C</p>
+                            <p>Cupboards</p>
+                            <Button
+                            text="Browse"
+                            bgColor={"bg-primary"}
+                            textColor={"text-white"}
+                            />
                         </div>
                     </div>
+                    <img src={Image1} alt="" className="w-[120px] absolute top-0 "/>
                 </div>
             </div>
         </div>
@@ -20,4 +28,4 @@ const Category = () => {
   )
 }
 
-export default Category
+export default Category;
