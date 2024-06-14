4801f77d1a69ed5949a259a657a4edfa3d7f48db
BAGABO-BONNY <bagabobonny544@gmail.com>
2024-06-15 01:52:01 +0200

    commit

--- shop/src/App.jsx
+++ shop/src/App.jsx
@@ -4,8 +4,21 @@ import Hero from './components/hero/hero';
 import Category from './components/Category/Category'
 import  Category2 from'./components/Category/Category2'
 import  Services from './components/Services/Services'
+import Banner from './components/Banner/Banner'
+import chair from './components/hero/ga.png'
 
+const BannerData ={
+  discount:"30% OFF",
+  title:"Fine Smile",
+  date:"10 jan to 29 jan",
+  Image:chair,
+  title2: "Lounge Chair",
+  title3:"Winter Sale",
+  title4:"lorem ipsum, dolor sit amet consecteur",
+  bgColor:"#f42c37",
+};
 const App = () => {
+  
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
       <Navbar/>
@@ -13,6 +26,7 @@ const App = () => {
       <Category/>
       <Category2/>
       <Services/>
+      <Banner data ={BannerData}/>
     </div>
   );
 };
